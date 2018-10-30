34eb44759416d241c590a0330301650d2ff4d8dd
Ryan Walsh <Ryan@1x-nat-vl931-172-30-143-107.wireless.umass.edu>
2018-10-30 13:11:07 -0400

    First audio input file, which just writes out decimal values for the detected USB audio input

---
+++ calibration/calibrate.py
@@ -0,0 +1,32 @@
+# Main calibration code
+
+import numpy
+import pyaudio
+
+import pyaudio
+import struct
+import numpy as np
+
+ 
+CHUNK = 1024 * 4         	# samples per frame
+FORMAT = pyaudio.paInt16 	# audio format (bytes per sample)
+CHANNELS = 1               	# single channel for microphone
+RATE = 44100                # samples per second
+
+ 
+ 
+p = pyaudio.PyAudio()
+ 
+stream = p.open(
+    format=FORMAT,
+    channels=CHANNELS,
+    rate=RATE,
+    input=True,
+    output=True,
+    frames_per_buffer=CHUNK
+)
+
+while True:
+    data = stream.read(CHUNK)					                #reading input	
+    data_int = struct.unpack(str(2 * CHUNK) + 'B', data) 	#converts bytes to integers
+    data_int	
