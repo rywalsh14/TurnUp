0c20fdd4f5f67a744a4bf8ec0ba8140350a03090
rywalsh14 <rywalsh14@gmail.com>
2019-02-25 22:59:32 -0500

    added basic app server files, created main folder to run device from

---
+++ main/threaded_server.py
@@ -0,0 +1,140 @@
+import socket
+import threading
+import json
+
+flowLock = threading.Lock()
+
+DEVICE_NAME = "TurnUp 1"
+SETTINGS_SERVER_PORT = 9999
+BUFFER_SIZE = 1024
+
+UDP_IP = '' # Empty to bind to any available interface
+UDP_PORT = 8111
+
+CLIENT_CONNECT_ADDRESS = None
+CLIENT_CONNECT_PORT = None  # Client port to send device data back to
+
+""" Basic TurnUp settings server flow:
+        1. Listen for a UDP device discovery message that will be broadcasted by the user phone
+            * discovery message will include a port number that the device can connect to in order to send important server data
+        2. Send device data to the user phone using the port number included in the broadcast message
+            * basically say to the phone "I am a turnup device, here's how to connect to me"
+        3. Listen for TCP connections that the phone can now connect to
+            * now the phone may connect and send user specified settings
+"""
+
+
+def UDPserver():
+    while True:
+        # UDP receive socket setup
+        UDPsocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        UDPsocket.bind((UDP_IP, UDP_PORT))
+        # Listen for the UDP broadcast message from the user phone
+        while True:
+            flowLock.acquire()
+            print("Starting UDP server to listen for discovery messages...")
+            print("Listening on port %s\n" % (UDP_PORT))
+            flowLock.release()
+            data, address = UDPsocket.recvfrom(4096)
+            try:
+                stringData = data.decode("UTF-8")       # decode the character bytes to a string
+                dataDict = json.loads(stringData)       # convert json string to a dictionary
+                CLIENT_CONNECT_PORT = dataDict["port"]  # extract the port number from the received data
+                CLIENT_CONNECT_ADDRESS = address[0]     # extract the client address from the socket data
+                if (CLIENT_CONNECT_ADDRESS is not None):
+                    flowLock.acquire()
+                    print("Received discovery message from %s:%d\n" % (CLIENT_CONNECT_ADDRESS, CLIENT_CONNECT_PORT))
+                    flowLock.release()
+                    break
+            except:     # ignore the UDP message and continue listening for discovery message
+                continue
+
+        
+        # create a response data dictionary
+        responseData = {
+            "name": DEVICE_NAME,
+            "port": SETTINGS_SERVER_PORT
+        }
+
+        # convert the dictionary to a byte representation of a JSON string
+        responseMessage = bytes(json.dumps(responseData), 'utf-8')
+
+        flowLock.acquire()
+        print("Constructed the following discovery response message: %s\n" % (responseMessage))
+        flowLock.release()
+
+        # Connect to the user phone using the recieved IP address and port
+        # Send back device connection data (device name & port to connect to) to the phone
+        responseSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # open socket to send response data to phone
+        responseSocket.connect((CLIENT_CONNECT_ADDRESS, CLIENT_CONNECT_PORT))   # connect socket to phone IP and port
+        responseSocket.sendall(responseMessage)
+        responseSocket.close()
+
+        flowLock.acquire()
+        print("Sent discovery response message to %s:%d" % (CLIENT_CONNECT_ADDRESS, CLIENT_CONNECT_PORT))
+        print("Completed discovery phase!\n")
+        flowLock.release()
+
+        # if no current TCP thread, start one
+        if len(threading.enumerate()) < 3:
+            TCPthread = threading.Thread(target=TCPserver, args=())
+            TCPthread.start()
+        
+
+
+
+def TCPserver():
+    # Set up the main device server to receive user settings data from a user phone
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", SETTINGS_SERVER_PORT))  # open up a socket that anyone may connect to
+    s.listen(1)
+    flowLock.acquire()
+    print("Starting TCP server to receive user commands")
+    print("Listening on port %s\n" % (SETTINGS_SERVER_PORT))
+    flowLock.release()
+    # Always look for connections
+    while 1:
+        conn, addr = s.accept()
+        flowLock.acquire()
+        print("Connected to client with address:port pair %s:%s\n" % (addr[0], str(addr[1])))
+        flowLock.release()
+        while 1:
+            # Receive the byte data
+            byteData = conn.recv(BUFFER_SIZE)
+            if not byteData:
+                break
+        
+        
+            stringData = byteData.decode("UTF-8")   # convert the byte data to a string
+            userMessage = json.loads(stringData)   # convert the string into a dictionary
+            
+            # settings, discovery, calibrate
+            
+            
+            # Two types of data to be sent... calibrate, or settings update
+            if userMessage["type"] == "calibrate":
+                # run calibration stage
+                flowLock.acquire()
+                print("Received CALIBRATE message from user\n")
+                flowLock.release()
+            elif userMessage["type"] == "settings":
+                # extrapolate and store the user settings in the system
+                flowLock.acquire()
+                print("Recived SETTINGS message from user")
+                print("Received the following settings:")
+                print("\tThreshold:\t%f" %(userMessage["threshold"]))
+                print("\tSensitivity:\t%d\n" %(userMessage["sensitivity"]))
+                flowLock.release()
+            else:
+                flowLock.acquire()
+                print("Received invalid message from user (neither calibrate nor settings message\n)")
+                flowLock.release()
+        conn.close()
+
+
+if __name__ == "__main__":
+    
+    UDPthread = threading.Thread(target=UDPserver, args=())
+
+    UDPthread.start()
+
